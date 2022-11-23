e4e1f5da683b8f299595893582f63efcbfadb19e
Rafael Veloso <rafaelveloso11@gmail.com>
2022-11-22 22:25:43 -0300

    edição demanda

--- lib/widget/card_widget.dart
+++ lib/widget/card_widget.dart
@@ -4,6 +4,7 @@ import 'package:intl/intl.dart';
 import 'package:smartcity/constants.dart';
 
 import '../model/task.dart';
+import 'form_widget.dart';
 
 class CardWidget extends StatelessWidget {
   final Task task;
@@ -15,85 +16,93 @@ class CardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Slidable(
-      key: const ValueKey(0),
-      startActionPane: ActionPane(
-        motion: const ScrollMotion(),
-        dismissible: DismissiblePane(onDismissed: () {}),
-        children: [
-          const SizedBox(
-            width: 2,
-          ),
-          SlidableAction(
-            borderRadius: BorderRadius.circular(16),
-            onPressed: (_) {},
-            backgroundColor: kPrimaryColor,
-            foregroundColor: kBackGroundColor,
-            icon: Icons.done_outlined,
-            label: 'Pronto',
-          ),
-          const SizedBox(
-            width: 2,
-          ),
-          SlidableAction(
-            borderRadius: BorderRadius.circular(16),
-            onPressed: (_) {},
-            backgroundColor: const Color(0xFFFE4A49),
-            foregroundColor: Colors.white,
-            icon: Icons.delete,
-            label: 'Remover',
-          ),
-        ],
-      ),
-      child: Card(
-        elevation: 8,
-        shadowColor: kBackGroundColor,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(
-            10,
-          ),
+        key: const ValueKey(0),
+        startActionPane: ActionPane(
+          motion: const ScrollMotion(),
+          dismissible: DismissiblePane(onDismissed: () {}),
+          children: [
+            const SizedBox(
+              width: 2,
+            ),
+            SlidableAction(
+              borderRadius: BorderRadius.circular(16),
+              onPressed: (_) {},
+              backgroundColor: kPrimaryColor,
+              foregroundColor: kBackGroundColor,
+              icon: Icons.done_outlined,
+              label: 'Pronto',
+            ),
+            const SizedBox(
+              width: 2,
+            ),
+            SlidableAction(
+              borderRadius: BorderRadius.circular(16),
+              onPressed: (_) {},
+              backgroundColor: const Color(0xFFFE4A49),
+              foregroundColor: Colors.white,
+              icon: Icons.delete,
+              label: 'Remover',
+            ),
+          ],
         ),
-        child: ListTile(
-          contentPadding: const EdgeInsets.symmetric(
-            vertical: 8,
-            horizontal: 16,
-          ),
-          minLeadingWidth: task.isDone ? 0 : 2,
-          leading: task.isDone
-              ? const SizedBox()
-              : Container(
-                  width: 2,
-                  color: kPrimaryColor,
+        child: GestureDetector(
+          onTap: () {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return const FormWidget();
+                });
+          },
+          child: Card(
+            elevation: 8,
+            shadowColor: kBackGroundColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(
+                10,
+              ),
+            ),
+            child: ListTile(
+              contentPadding: const EdgeInsets.symmetric(
+                vertical: 8,
+                horizontal: 16,
+              ),
+              minLeadingWidth: task.isDone ? 0 : 2,
+              leading: task.isDone
+                  ? const SizedBox()
+                  : Container(
+                      width: 2,
+                      color: kPrimaryColor,
+                    ),
+              title: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Text(
+                  task.taskName,
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-          title: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Text(
-              task.taskName,
-              style: const TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
+              ),
+              subtitle: Text(
+                task.description,
+                // ignore: prefer_const_constructors
+                style: TextStyle(
+                  color: kPrimaryColor,
+                  fontSize: 16,
+                ),
+              ),
+              trailing: Text(
+                task.isDone
+                    ? 'Concluído'
+                    // ignore: prefer_interpolation_to_compose_strings
+                    : DateFormat('dd/MM/yyyy').format(task.taskTime),
+                style: const TextStyle(
+                  color: kPrimaryColor,
+                  fontSize: 16,
+                ),
               ),
             ),
           ),
-          subtitle: Text(
-            task.description,
-            // ignore: prefer_const_constructors
-            style: TextStyle(
-              color: kPrimaryColor,
-              fontSize: 16,
-            ),
-          ),
-          trailing: Text(
-            task.isDone
-                ? 'Concluído'
-                // ignore: prefer_interpolation_to_compose_strings
-                : DateFormat('dd/MM/yyyy').format(task.taskTime),
-            style: const TextStyle(
-              color: kPrimaryColor,
-              fontSize: 16,
-            ),
-          ),
-        ),
-      ),
-    );
+        ));
   }
 }
